3e051c845278ed68a5402e1a0ba866252ca28d7f
hareeshcdebatch17 <138010844+hareeshcdebatch17@users.noreply.github.com>
2023-06-28 22:39:12 +0530

    Add files via upload
    files added

---
+++ python.py
@@ -0,0 +1,4 @@
+this is [1;6D[1;6D[1;6D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cthis is my python file [1;6Dfor i in range(5,0,-1):
+    print('*'*i)
+for i in range(5,0,-1):
+    print('*'*i)
